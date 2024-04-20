252374506e074b32eb09620f31c29feed9a296d6
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-04-20 21:20:53 +0200

    feature: add try-except

--- api/book_session.py
+++ api/book_session.py
@@ -6,14 +6,17 @@ from booking import book
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        users = os.environ.get("USERS")
-        if users:
-            users_split = users.split(",")
-            for user in users_split:
-                user_split = user.split(":")
-                name = user_split[0]
-                key = user_split[1]
-                book(key, name)
+        try:
+            users = os.environ.get("USERS")
+            if users:
+                users_split = users.split(",")
+                for user in users_split:
+                    user_split = user.split(":")
+                    name = user_split[0]
+                    key = user_split[1]
+                    book(key, name)
+        except Exception as e:
+            print(f"Error executing lambda: {e}")
 
         self.send_response(200)
         return
