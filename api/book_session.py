8bb58d671618cc48a42dfe5d5ad029687ba0f00c
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-04-20 23:01:55 +0200

    fix lambda response

--- api/book_session.py
+++ api/book_session.py
@@ -6,17 +6,15 @@ from booking import book
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        try:
-            users = os.environ.get("USERS")
-            if users:
-                users_split = users.split(",")
-                for user in users_split:
-                    user_split = user.split(":")
-                    name = user_split[0]
-                    key = user_split[1]
-                    book(key, name)
-        except Exception as e:
-            print(f"Error executing lambda: {e}")
-
         self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        users = os.environ.get("USERS")
+        if users:
+            users_split = users.split(",")
+            for user in users_split:
+                user_split = user.split(":")
+                name = user_split[0]
+                key = user_split[1]
+                book(key, name)
         return
