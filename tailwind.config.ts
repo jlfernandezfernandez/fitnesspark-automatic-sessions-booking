ca532cdfe201f5fbacad7327c14a57feffb67ee2
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-05-12 17:18:22 +0200

    fix: restore text animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,18 @@ const config: Config = {
       animation: {
         text: "text 5s ease infinite",
       },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
